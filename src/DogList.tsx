e598bba57ca08d0cff7f06db79569282bce8a9a0
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-21 17:07:55 -0500

    Add dog default info

--- src/DogList.tsx
+++ src/DogList.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import * as whiskey from "./whiskey.jpg";
 
 function DogList(): JSX.Element {
-  return <div className="DogList">All dogs</div>;
+  return (
+    <div className="DogList">
+      All dogs
+      <img src={whiskey.default} alt="" />
+    </div>
+  );
 }
 
 export default DogList;
